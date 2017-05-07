afabfaaca5e36b6c43cfae48848ef5666c696df5
mhoskins90 <hoskins.matt@gmail.com>
2017-05-07 10:36:24 -0400

    Add files via upload

---
+++ htmlCreation.py
@@ -0,0 +1,76 @@
+
+
+document_name = input('What do you want the document name to be?: ')
+file_name = "{0}{1}".format(document_name, ".html")
+
+INITIAL_HTML_TAGS = '\
+<!doctype html>\n\
+<html>\n\
+<head>\n\
+<meta charset="utf-8">\n\
+'
+
+TITLE = '<title>{0}</title>\n'.format("starting text".lower().title())
+KEYWORDS = '<meta name="keywords" content="{0}" />\n'.format("Starting, Text")
+DESCRIPTION = '<meta name="description" content="{0}" />\n'.format("starting text".capitalize())
+JQUERY_LINK = 'https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js'
+JQUERY_LINK_TAG = '<script src="{0}"></script>'.format(JQUERY_LINK)
+JQUERY_DOCUMENT_READY_FUNCTION ='\
+<script type="text/javascript">\n\
+$(document).ready(function() {\n\
+\n\
+//SCRIPT HERE\n\
+\n\
+}); //END DOCUMENT READY\n\
+</script>\
+'
+
+JAVASCRIPT_YEAR_FUNCTION = '<script>\n\
+function getCurrentYear() { // YEAR FUNCTION\n\
+var d = new Date();\n\
+Year = d.getFullYear();\n\
+document.write(Year);\n\
+</script>\n\
+'
+
+CLOSINGHEAD_OPENINGBODY_HEADER_NAV = '</head>\n\
+<body>\n\
+<header><!--HEADER GOES HERE--></header>\n\
+<nav><!--NAVIGATION GOES HERE--></nav>\
+\
+'
+BODY_CONTENT = '<h1>Starting Text</h1>\n\
+<p> Starting text in sentence </p>\n\
+'
+
+FOOTER = '<footer>{0} Copyright Matt Hoskins <script type="text/javascript">getCurrentYear();</script>.</footer>\n\
+'.format("&copy;")
+
+ENDING_HTML_TAGS = '</body>\n\
+</html>'
+
+
+HTML_PAGE_ELEMENTS = {
+	1:INITIAL_HTML_TAGS,
+	2:TITLE,
+	3:KEYWORDS,
+	4:DESCRIPTION,
+	5:JQUERY_LINK_TAG,
+	6:JQUERY_DOCUMENT_READY_FUNCTION,
+	7:JAVASCRIPT_YEAR_FUNCTION,
+	8:CLOSINGHEAD_OPENINGBODY_HEADER_NAV,
+	9:BODY_CONTENT,
+	10:FOOTER,
+	11:ENDING_HTML_TAGS
+	}
+
+created_file=open(file_name, "w")#PUT ALL DATA TO HTML PAGE
+
+for key,value in HTML_PAGE_ELEMENTS.items():
+	created_file.write(value)
+
+
+created_file.close()#CLOSE IT DOWN
+
+print("File Created")
+#print(HTML_ELEMENTS["start"])
---
+++ textEditor.py
@@ -0,0 +1,64 @@
+from tkinter import *
+import os, sys, tkinter.filedialog
+from tkinter.filedialog import askopenfilename
+
+
+class EditorProgram(Tk):
+	def __init__(self,type):
+		self.type = type
+
+
+	def run_editor(self):
+		container=Tk()
+		container.title("HoskinsEditor")
+		container.configure(background='#f2f2f2')
+		global text
+		text=Text(container) 
+		text.configure(background='#202020', foreground='#00FF7C')
+
+		text.grid(row=0,columnspan=30) 
+
+
+		button=Button(container, text=" Save ", command=self.saveas)
+		button.configure(background='green', foreground='white', cursor="hand2") 
+		button.grid(row=1, column=14)
+
+		quit_button=Button(container, text="Close", command=container.quit) 
+		quit_button.configure(background='red', foreground='white', cursor="hand2")
+		quit_button.grid(row=1, column=15)
+		try:
+			container.mainloop()
+		
+		except Exception as error:
+			print("Error:", error)
+		else:
+			print('Goodbye, come back soon!')
+	def saveas(self):
+
+		global text
+	
+		t = text.get("1.0", "end-1c")
+		t +='\n\n'+'-'*50+'\n'+'-'*50+'\n Created With MH_Editor'
+
+		try:
+			print('Save Dialog Initiated...')
+			savelocation=tkinter.filedialog.asksaveasfilename(defaultextension=".mh", filetypes=(("HoskinsEditor Document", "*.mh"),("Text Document", "*.txt"),\
+				("Python Document", "*.py"),("C Document", "*.c"),("HTML Document", "*.html"),("PHP Document", "*.php"),("All Files", "*.*") ))
+			file=open(savelocation, "w+")
+			file.write(t)
+			file.close()
+				
+		except (FileNotFoundError, Exception) as error:
+			print ('Save Dialog Exited Without Saving.'.format(error))
+		else:
+			print('File Saved Successfully!')
+
+
+
+def main():
+	regular = EditorProgram("Regular")
+	regular.run_editor()
+
+
+if __name__ == "__main__":
+	main()
